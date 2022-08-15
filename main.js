5a6147a1acd79edb7eae7a23c4b5aabb464ecaca
edyleudo <edyleudo@hotmail.com>
2022-08-15 14:52:11 -0300

    errolocao1

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function tocaSom (seletorAudio){
         console.log('Elemento não encontrado ou seletor invalido');
     }
 }
-//erropelogit1
+//errolocal1
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
