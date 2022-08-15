4cfb1c9a83e0bb990f1151c776c6dd92478befbf
edyleudo <edyleudo@hotmail.com>
2022-08-15 14:56:05 -0300

    errolocal3

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function tocaSom (seletorAudio){
         console.log('Elemento não encontrado ou seletor invalido');
     }
 }
-//errolocal1
+//errolocal3
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
