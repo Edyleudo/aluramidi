bd2aefea8c29d9b23c6f1b0a4688bb54cb29a170
edyleudo <edyleudo@hotmail.com>
2022-08-15 14:17:35 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+function tocaSom (seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio'){ //localname pela o nome do local do elemento, função do js
+        elemento.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor invalido');
+    }
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //template string
+    const idaudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idaudio);
+    }
+
+    tecla.onkeydown = function(evento){
+        if(evento.code == 'Space' || evento.code == 'Enter') // code pega o codigo daquela tecla, função do js
+            tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
+
+    contador++;
+
+}
