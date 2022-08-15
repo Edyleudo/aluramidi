e6267b8af91e5eef042916c42dffbe413f3dd836
Edyleudo Markys <edyleudo@hotmail.com>
2022-08-15 14:33:57 -0300

    teste2

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function tocaSom (seletorAudio){
         console.log('Elemento não encontrado ou seletor invalido');
     }
 }
-//testandosemerro
+//testandoerro123
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
