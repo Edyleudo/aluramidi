45b66ca7d0987813e4e19f607e240658758d90c1
Edyleudo Markys <edyleudo@hotmail.com>
2022-08-15 15:00:34 -0300

    errogit4

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function tocaSom (seletorAudio){
         console.log('Elemento não encontrado ou seletor invalido');
     }
 }
-//errolocal3
+//errogit4
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
